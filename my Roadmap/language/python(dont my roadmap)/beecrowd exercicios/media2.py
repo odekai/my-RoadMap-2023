542ffdabf32197ff0f791ad3796276ce2b015799
odekai <vilasjoaovitor71@gmail.com>
2024-04-17 15:35:15 -0300

    att2024

---
+++ my Roadmap/language/python(dont my roadmap)/beecrowd exercicios/media2.py
@@ -0,0 +1,9 @@
+A=round(float(input()))
+B=round(float(input()))
+C=round(float(input()))
+#pesos 2,3,5
+media_final=(A * 2 + B * 3 + C * 5) / (2+3+5)
+print("MEDIA = {:.1f} ".format(media_final))
+
+
+
